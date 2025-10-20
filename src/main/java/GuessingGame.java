a38512cf82dc630b9cab81f350ec1d3f1d9f2c38
sumaya-sid <sumaya.siddiqui49024@gmail.com>
2025-10-20 13:29:03 -0400

    Update GuessingGame.java

--- src/main/java/GuessingGame.java
+++ src/main/java/GuessingGame.java
@@ -1,9 +1,34 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class GuessingGame {
     public static void main(String[] args) {
         // TODO: Prompt the user with a welcome message
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        System.out.println("Welcome to the Guessing Game!");
+        System.out.println("I am thinking of a number between 1 and 100.");
         // TODO: Generate a random secret number between 1 and 100 (inclusive)
+        int number = rand.nextInt(101);
+        boolean run = true;
+        int guess;
+        do
+        {
+            System.out.print("Enter your guess: ");
+            guess = sc.nextInt();
+            if (guess > number)
+            {
+                System.out.println("Too high! Try again.");
+            }
+            if (guess < number)
+            {
+                System.out.println("Too low! Try again.");
+            }
+        }
+        while (guess != number);
+        System.out.println("You got it!");
+
         // TODO: Use a do...while loop to read guesses until correct
         // TODO: Print "Too low" / "Too high" / "You got it!" accordingly
         // TODO: Use Scanner for input
